6c9782fa6fb351f751b8d4d2438bea7b9c55b2b1
xiaobin3 <xiaobin3@asiainfo.com>
2018-04-16 17:08:32 +0800

    new commit

---
+++ EEWebCase/src/main/java/com/xiaobin/transaction/transfer/web/TransferServlet.java
@@ -0,0 +1,41 @@
+package com.xiaobin.transaction.transfer.web;
+
+
+import com.xiaobin.transaction.transfer.service.TransferService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TransferServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//接受转账的参数
+		String out = request.getParameter("out");
+		String in = request.getParameter("in");
+		String moneyStr = request.getParameter("money");
+		double money = Double.parseDouble(moneyStr);
+		
+		//调用业务层的转账方法
+		TransferService service = new TransferService();
+		boolean isTransferSuccess = service.transfer(out,in,money);
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		if(isTransferSuccess){
+			response.getWriter().write("转账成功！！！");
+		}else{
+			response.getWriter().write("转账失败！！！");
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
