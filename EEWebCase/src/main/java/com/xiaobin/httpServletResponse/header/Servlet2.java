9ae445e5f22c1fbf8a814bc42a91af2d100dda92
xiaobin <xiaoinwater@163.com>
2018-04-17 23:59:00 +0800

    new commit

--- EEWebCase/src/main/java/com/xiaobin/httpServletResponse/header/Servlet2.java
+++ EEWebCase/src/main/java/com/xiaobin/httpServletResponse/header/Servlet2.java
@@ -1,6 +1,7 @@
 package com.xiaobin.httpServletResponse.header;
 
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,17 @@ public class Servlet2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		Date date = new Date();
+
+		//设置响应头
+		response.addHeader("name", "zhangsan");
+		//response.addIntHeader("age", 28);
+		//response.addDateHeader("birthday", date.getTime());
+
+		response.addHeader("name", "lisi");
+
+		response.setHeader("age", "28");
+		response.setHeader("age", "50");
 		response.setContentType("text/html;charset=UTF-8");
 		//设置定时刷新的头 如下servlet1重定向到servlet2，servlet2中定时刷新servlet1，就形成了循环刷新
 //		response.setHeader("refresh", "5;url=http://localhost:8080/servlet1");
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mavenEEWeb
 mavenEEWeb
+
+#页面素材
+`http://www.17sucai.com/`
